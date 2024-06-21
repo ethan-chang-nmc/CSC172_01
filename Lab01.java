d0e8027e228c914dba1ca12381622894f62198e0
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2024-06-21 14:36:51 -0400

    Create Lab01.java

---
+++ Lab01.java
@@ -0,0 +1,29 @@
+/*
+*Ethan Chang
+*echang28@u.rochester.edu
+*
+*07/07/2024: Implementing a print array and get maximum function
+*/
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Integer [] intArry = {1, 2, 3, 4, 5 };
+        Double [] doubArry = {1.1, 2.2, 3.3, 4.4};
+        Character [] charArray = {'H','E','L', 'L', 'O' };
+        String [] strArray = {"once", "upon", "a", "time" };
+        printArray(intArry);
+        printArray(doubArry);
+        printArray(charArray);
+        printArray(strArray);
+    }
+
+    /*
+    *Implementation of the print array function, which itterates through an inputed array and prints each element
+    */
+    public static void printArray(Object array)
+    {
+        f
+    }
+}
