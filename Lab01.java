788965ab6b5ba06334f9e4b626ae4336be320582
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2024-07-04 23:54:37 -0400

    Update Lab01.java

--- Lab01.java
+++ Lab01.java
@@ -4,6 +4,7 @@
 *
 *07/07/2024: Implementing a print array and get maximum function
 */
+import java.util.function.Function;
 
 public class Lab01 
 {
@@ -18,23 +19,36 @@ public class Lab01
         printArray(doubArry);
         printArray(charArry);
         printArray(strArry);
-        /*problems 4-6*/
+        
+        /*problems 4-5*/
         System.out.println("max Integer is: " + getMax(intArry));
         System.out.println("max Double is: " + getMax(doubArry));
         System.out.println("max Character is: " + getMax(charArry));
         System.out.println("max String is: " + getMax(strArry)) ;
+
+        /*creating a function for max character*/
+        Function<Character[], Character> findMax = (Character[] inpArray) -> { //map char array to char
+            Character max = inpArray[0]; //set max char to first char
+            for (int i = 1; i < inpArray.length; i++) { //iterrates through char array and replaces max with larger char
+                if (inpArray[i].compareTo(max) > 0) {
+                    max = inpArray[i];
+                }
+            }
+            return max;
+        };
+        System.out.println("max Double is: " + findMax.apply(charArry));
     }
 
     /*
     *Implementation of the getMax function, which iterates through an input array and returns the max value
     */
-    /*SECOND IMPLEMENTATION OF getMas WITH GENERICS */
+    /*SECOND IMPLEMENTATION OF getMax WITH GENERICS */
     public static <T extends Comparable<T>> T getMax(T[] anArray)
     {
-        T maxval = anArray[0];
-        for (int i = 1; i < anArray.length; i++)
+        T maxval = anArray[0]; //getting the first value in the array
+        for (int i = 1; i < anArray.length; i++) //iterrating through the array to the end
         {
-            if (anArray[i].compareTo(maxval)>0)
+            if (anArray[i].compareTo(maxval)>0) //comparing two values and storing the greater value into maxval
             {
                 maxval = anArray[i];
             }
@@ -43,15 +57,16 @@ public class Lab01
     }
 
     /*FIRST IMPLEMENTATION OF getMax WITHOUT GENERICS
-    *Errors: Exception in thread "main" java.lang.Error: Unresolved compilation problems: 
-        charArry cannot be resolved to a variable
-        strArry cannot be resolved to a variable
+    *Errors:
+    *Comparable is a raw type. References to generic type Comparable<T> should be parameterized
+    *Type safety: The method compareTo(Object) belongs to the raw type Comparable. References to generic type Comparable<T> should be parameterized
+    
     public static Comparable getMax(Comparable[] anArray)
     {
-        Comparable maxval = anArray[0];
-        for (int i = 1; i < anArray.length; i++)
+        Comparable maxval = anArray[0]; //getting the first value in the array
+        for (int i = 1; i < anArray.length; i++) //iterrating through the array to the end
         {
-            if (anArray[i].compareTo(maxval)>0)
+            if (anArray[i].compareTo(maxval)>0) //comparing two values and storing the greater value into maxval
             {
                 maxval = anArray[i];
             }
@@ -73,7 +88,8 @@ public class Lab01
         System.out.println(); //new line for next time it is called
     }
     
-    /*SECOND IMPLEMENTATION OF printArray USING MEHTOD OVERLOADING
+    /*SECOND IMPLEMENTATION OF printArray USING METHOD OVERLOADING
+    
     public static void printArray(Integer[] array) 
     {
         for (int element : array) //go through each element in the array
@@ -109,13 +125,14 @@ public class Lab01
     */
     
     /*FIRST IMPLEMENTATION USING OBJECT
+
     public static void printArray(Object[] array)
     {
         for (Object element : array) //go through each element in the array
         {
             System.out.print(element + " "); //print out that element, seperated by spaces
         }
-        System.out.println(); //new line for next time it is called
+        System.out.println(); //new line
     }
     */
 }
